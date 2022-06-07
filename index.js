a94d61cf7248f8ec4b580739ac91888c16f3379c
Ricky Pickens <narutoleo15@yahoo.com>
2022-06-07 00:01:39 -0500

    player and enemy created

--- index.js
+++ index.js
@@ -6,22 +6,31 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
     constructor({ position, velocity },) {
-        this.position = position
-        this.velocity = velocity
-    }
+        this.position = position;
+        this.velocity = velocity;
+        this.height = 150;
+    };
 
     draw() {
         c.fillStyle = "blue"
-        c.fillRect(this.position.x, this.position.y, 50, 150)
-    }
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    };
 
     update() {
-        this.draw()
-        this.position.y += 10
-    }
-}
+        this.draw();
+        this.position.y += this.velocity.y;
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+
+        } else this.velocity.y += gravity;
+
+
+    };
+};
 
 
 const player = new Sprite({
@@ -31,7 +40,7 @@ const player = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 0
+        y: 10
     }
 });
 
