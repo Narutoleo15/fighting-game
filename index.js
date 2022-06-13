c05fdc64f3992b2aedce4b903425f50104a5f666
Ricky Pickens <narutoleo15@yahoo.com>
2022-06-12 19:22:33 -0500

    added in shop sprite

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ const background = new Sprite({
     },
     imageSrc: './img/background.png'
 })
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 127
+    },
+    imageSrc: './img/shop.png',
+    scale: 2.75,
+    framesMax: 6
+})
 
 const player = new Fighter({
     position: {
@@ -65,6 +74,62 @@ const keys = {
 
 
 decTimer()
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    background.update()
+    shop.update()
+    player.update();
+    enemy.update();
+
+
+    player.velocity.x = 0
+    // player movement
+    if (keys.a.pressed && player.lastKey === "a") {
+        player.velocity.x = -5
+    } else if (keys.d.pressed && player.lastKey === "d") {
+        player.velocity.x = 5
+    };
+
+    enemy.velocity.x = 0
+    // enemy movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
+        enemy.velocity.x = -5
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
+        enemy.velocity.x = 5
+    };
+
+    // detect for collision
+    if (
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy
+        }) &&
+        player.isAttacking
+    ) {
+        player.isAttacking = false;
+        enemy.health -= 20
+        document.querySelector("#enemyHealth").style.width = enemy.health + "%"
+    };
+    if (
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player
+        }) &&
+        enemy.isAttacking
+    ) {
+        enemy.isAttacking = false;
+        player.health -= 20
+        document.querySelector("#playerHealth").style.width = player.health + "%"
+    };
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId })
+    }
+};
+
+
 animate()
 
 window.addEventListener("keydown", (event) => {
